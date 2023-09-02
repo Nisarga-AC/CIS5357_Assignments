95ac9919c05f43cbf39a85462fb41409139f6c9d
Nisarga-AC <nisarganisse@gmail.com>
2023-09-01 23:19:30 -0500

    adding new line

--- Assignment2/M2_coding_assignment.py
+++ Assignment2/M2_coding_assignment.py
@@ -30,3 +30,4 @@ print("<Approach 2>\n")
 print(
     f" My Favorite song is {my_fav_song} and \n it was sung by one of my favorite's singer {my_fav_singer} \n and my most favorite line in that song is {fav_line_of_the_song}"
 )
+print("adding new line")
