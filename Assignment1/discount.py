0e295998025c9c4203fe9c688fd1f90870ff5e34
Nisarga-AC <nisarganisse@gmail.com>
2023-09-01 23:56:21 -0500

    adding new line

--- Assignment1/discount.py
+++ Assignment1/discount.py
@@ -1,10 +1,12 @@
-Price = int(input("what is the price of this Doll? ")) # Here asks a user for a price
-discount1 = int(0.1 * Price) # 10 percent is calculated
-final1= Price - discount1
+Price = int(input("what is the price of this Doll? "))  # Here asks a user for a price
+discount1 = int(0.1 * Price)  # 10 percent is calculated
+final1 = Price - discount1
 print(f" The price of the Doll after 10% discount is $ {final1}")
-discount2 = int(0.15* Price) # 15 percent is calculated
+discount2 = int(0.15 * Price)  # 15 percent is calculated
 final2 = Price - discount2
 print(f" The price of the Doll after 15% discount is ${final2}")
-discount3 =int(0.20*Price) # 20 percent is calculated
-final3= Price - discount3
-print(f'The price of the doll after 20% discount is ${final3}')
+discount3 = int(0.20 * Price)  # 20 percent is calculated
+final3 = Price - discount3
+print(f"The price of the doll after 20% discount is ${final3}")
+
+Print("done")
