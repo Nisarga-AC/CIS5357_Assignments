8ecbd1ae3737ca2e3e832ad1f281d8fc4a4a0236
Nisarga-AC <nisarganisse@gmail.com>
2023-09-01 23:58:48 -0500

    changes cancelled

--- Assignment1/discount.py
+++ Assignment1/discount.py
@@ -8,5 +8,3 @@ print(f" The price of the Doll after 15% discount is ${final2}")
 discount3 = int(0.20 * Price)  # 20 percent is calculated
 final3 = Price - discount3
 print(f"The price of the doll after 20% discount is ${final3}")
-
-Print("done")
