68026dfb81a0c319a35d3dea2a7b66371f7a5f68
Nisarga-AC <nisarganisse@gmail.com>
2023-09-01 23:13:51 -0500

    First commit - assignments

---
+++ Assignment2/M2_coding_assignment.py
@@ -0,0 +1,32 @@
+"""
+My Name is Nisarga Arsikere Chidananda
+My Student Id is A05289369
+"""
+
+# Lets consider User as Harsha and will ask his Favorite song
+harsha_fav_song = input(" Hello Harsha May I know what is your favorite Song? \n")
+print(f"Favorite song of Harsha is {harsha_fav_song}\n")
+
+# My Favorite song details is explained below
+"""
+I like this song so much because this song is on Lord Shiva.
+This Song is about a rural woman expressing her love and devotion for Lord Shiva in her own simple way.
+I love to listen devotional Kannada songs.
+This song is in Kannada language and its my Native language.
+"""
+print("<====My Favorites====>")
+my_fav_song = "Sojugaada sooju mallige"
+my_fav_singer = "Ananya Bhat"
+fav_line_of_the_song = "Chandakki maale bilpatre tulasi dalava"
+
+# One of the Possible ways of printing my Favorite's
+print("<Approach 1>\n")
+print(f"My Favorite Song is {my_fav_song}")
+print(f"My Favorite Singer is {my_fav_singer}")
+print(f"My Favorite Line in the song is {fav_line_of_the_song} \n")
+
+# Another approach for expressing my Favorite's
+print("<Approach 2>\n")
+print(
+    f" My Favorite song is {my_fav_song} and \n it was sung by one of my favorite's singer {my_fav_singer} \n and my most favorite line in that song is {fav_line_of_the_song}"
+)
