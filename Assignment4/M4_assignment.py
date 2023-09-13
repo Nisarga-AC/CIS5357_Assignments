789b9153c4d37455b1e697dba26f76a59c545b52
Nisarga-AC <nisarganisse@gmail.com>
2023-09-13 14:35:47 -0500

    updated some changes and added the new file

--- Assignment4/M4_assignment.py
+++ Assignment4/M4_assignment.py
@@ -16,9 +16,14 @@ for number in M4_assignment:
  if the condition is False the number will append/add to the odd_list
 
 """
-print("Approach 1 Output:")
+print("Approach 1 Output: \n")
 print(f"odd numbers are {odd_list}")  # printing the odd_list
-print(f"even numbers are {even_list}\n")  # printing the even_list
+print(f"even numbers are {even_list}")  # printing the even_list
+
+# printing the number(count) of odd and even no's in the list
+print("The no of even numbers in the list are ", len(even_list))
+print("The no of odd numbers in the list are ", len(odd_list))
+print("\n")
 
 
 # Approach 2
@@ -29,9 +34,15 @@ even_list = []  # initializing the empty even list
 for number in M4_assignment:
     even_list.append(number) if number % 2 == 0 else odd_list.append(number)
 
-odd_list.sort()  # Sorting the odd_list in ascending order by calling the sort function
-even_list.sort()  # Sorting the even_list in ascending order by calling the sort function
+# Sorting the odd_list in descending order by calling the sort function
+odd_list.sort(reverse=True)
+# Sorting the even_list in ascending order by calling the sort function
+even_list.sort()
 
-print("Approach 2 Output:")
+print("Approach 2 Output:\n")
 print("odd numbers are", odd_list)  # printing the odd_list
 print("even numbers are", even_list)  # printing the even_list
+
+# printing the number(count) of odd and even no's in the list
+print("The no of even numbers in the list are ", len(even_list))
+print("The no of odd numbers in the list are", len(odd_list))
